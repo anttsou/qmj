e4a6da99578f675a19f5ef85924156c5bc0c7dc1
Anthoney Tsou <anttsou@gmail.com>
2015-01-13 16:08:19 -0500

    added function to get companies

---
+++ getcompanies.R
@@ -0,0 +1,26 @@
+getcompanies <- function() {
+  i <- 1
+  newLetters <- c("num",letters)
+  stringfront <- "http://www.investorguide.com/stock/stocklist"
+  stringback <- ".html"
+  names <- NULL
+  tickers <- NULL
+  while(i <= length(newLetters)) {
+    newUrl <- paste(stringfront,paste(letters[i],stringback,sep=""),sep="")
+    con <- url(newUrl)
+    htmlCode <- readLines(con)
+    close(con)
+    n <- 238
+    while(htmlCode[n] != "<div class=\"stock-char-links\">") {
+      if(htmlCode[n] != "N/A</div>") {
+        newString <- sub('</div>','',htmlCode[n])
+        newTicker <- sub("\">.*","",sub(".*ticker=","",htmlCode[n+3]))
+        names <- c(names,newString)
+        tickers <- c(tickers,newTicker)
+      }
+      n = n+16
+    }
+    i = i+1
+  }
+  data.frame(names = names, tickers = tickers)
+}
