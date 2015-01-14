c9f9fec75748ccbaa79da4846eeea66095207d1a
Ryan Kwon <rynkwn@gmail.com>
2015-01-14 11:13:47 -0500

    Added n parameter that allows the user to retrieve the data from at most n random companies. Default set to 5000

--- R/getdailydata.R
+++ R/getdailydata.R
@@ -1,11 +1,17 @@
-getdailydata <- function(x){
+getdailydata <- function(x, n=5000){
   ##x is a dataframe containing a list of companies.
   ##Requires quantmod package
   ##getdailydata is meant to be used only when desiring to retrieve
   ### data from the web.
   numCompanies <- length(x$tickers)
-  for(i in 1:numCompanies){
-    companyTicker <- as.character(x$ticker[i])
+  if(n > numCompanies){
+    errorMessage <- paste("Error, at most only ", n, " companies may be chosen.", sep='')
+    stop(errorMessage)
+  }else{
+    randomNums <- sample(1:numCompanies, n, replace=FALSE) 
+  }
+  for(i in 1:n){
+    companyTicker <- as.character(x$ticker[randomNums[i]])
     stockData <- tryCatch(
       quantmod::getSymbols(companyTicker, src="google", auto.assign=FALSE),
       error=function(e) e
