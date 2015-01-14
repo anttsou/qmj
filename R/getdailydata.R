74ac76af89abeb798b0f565b4d80f230e1710195
Ryan Kwon <rynkwn@gmail.com>
2015-01-14 11:04:01 -0500

    Editted daily data to solely return closing stock values. Removed the check for quantmod in the code.

--- R/getdailydata.R
+++ R/getdailydata.R
@@ -3,20 +3,19 @@ getdailydata <- function(x){
   ##Requires quantmod package
   ##getdailydata is meant to be used only when desiring to retrieve
   ### data from the web.
-  if(!require("quantmod")){
-   stop("quantmod package missing for function getdailydata() in package qmj")
-  } else{
-    numCompanies <- length(x$tickers)
-    for(i in 1:numCompanies){
-      companyTicker <- as.character(x$ticker[i])
-      stockData <- tryCatch(
-        quantmod::getSymbols(companyTicker, src="google", auto.assign=FALSE),
-        error=function(e) e
-      )
-      if(!inherits(stockData, "error")){
-        fileName <- paste("data/", companyTicker, ".csv", sep='')
-        write.zoo(stockData, file = fileName, sep=",") 
-      }
+  numCompanies <- length(x$tickers)
+  for(i in 1:numCompanies){
+    companyTicker <- as.character(x$ticker[i])
+    stockData <- tryCatch(
+      quantmod::getSymbols(companyTicker, src="google", auto.assign=FALSE),
+      error=function(e) e
+    )
+    if(!inherits(stockData, "error")){
+      stockData <- stockData[,4]
+      fileName <- paste("data/", companyTicker, ".csv", sep='')
+      write.zoo(stockData, file = fileName, sep=",") 
+    } else{
+      print(paste("Error retrieving data for ", companyTicker, sep=""))
     }
   }
 }
