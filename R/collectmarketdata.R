8293dfb16dc59eb218d521b8b12c7decf61cbad1
Ryan Kwon <rynkwn@gmail.com>
2015-01-14 14:57:37 -0500

    Found data needed for profitability calculation, and commented where they may be found. Created overall structure of function.

--- R/collectmarketdata.R
+++ R/collectmarketdata.R
@@ -12,8 +12,46 @@ collectmarketdata <- function(x){
   #   i.e., low accruals, ACC
   
   numCompanies <- length(x$tickers)
-  #GPOA = (revenue - cost of goods sold)/(total assets)
-  #Cost of goods sold = Beginning Inventory + Inventory Purchases - End Inventory
-  ##Total assets - in balance sheet.
-  
+  profitability <- rep(0, numCompanies)
+  for(i in 1:numCompanies){
+    #GPOA = (revenue - cost of goods sold)/(total assets)
+    #?#GROSS PROFITS OVER TOTAL ASSETS. THIS CAN BE EASILY FOUND.
+    #Cost of goods sold = Beginning Inventory + Inventory Purchases - End Inventory
+    ##Gross profit - Income statement
+    ##Total assets - in balance sheet.
+    
+    #ROE
+    # Net income /book equity
+    # Net income - Cash flow
+    #?#Book equity = Total equity (BS) 
+    
+    #ROA
+    #Net income / Total assets
+    # Net income - CF
+    # Total assets - BS
+    
+    #CFOA
+    #(net income + depreciation - (change in working capital) - capital expenditures)/(total assets)
+    # Net income - CF
+    # Depreciation - IS
+    # Change in working capital - CF
+    # Capital Expenditures - CF
+    # Total assets - BS
+    
+    #GMAR
+    # (Revenue - costs of goods sold)/(total sales)
+    # = Gross profit/(total sales)
+    #?# Using different equation:
+    # Gross profit/ (Total revenue)
+    #Gross profit - IS
+    #Total Revenue - IS
+    
+    #ACC
+    # (depreciation - changes in working capital)/(total assets)
+    #*# Going from equation they show. Slight difference from their own
+    ## words.
+    # Depreciation - CF
+    # Changes in working capital - CF
+    # Total assets - BS 
+  }
 }
