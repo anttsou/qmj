41dde372ee605834cdc56a35e1e094a31d69051d
Ryan Kwon <rynkwn@gmail.com>
2015-01-16 13:44:42 -0500

    Created structure and functionality for helper function for collectmarketdata that collects and returns a vector of profitability z-scores for a given list of companies.

--- R/collectmarketdata.R
+++ R/collectmarketdata.R
@@ -1,4 +1,4 @@
-collectmarketdata <- function(x){
+collectmarketdata <- function(){
   ##Collect market data focuses on collecting needed
   ##  means and sd's for use in other functions.
   
@@ -10,66 +10,36 @@ collectmarketdata <- function(x){
   # Gross margin (GMAR)
   # Fraction of earnings composed of cash
   #   i.e., low accruals, ACC
+  companies <- read.csv("companies.csv")
+  numCompanies <- length(companies$tickers)
+  BS <- read.csv("balancesheets.csv")
+  CF <- read.csv("cashflows.csv")
+  IS <- read.csv("incomestatements.csv")
   
-  numCompanies <- length(x$tickers)
-  profitability <- rep(0, numCompanies)
+  #What to do with missing data?
+  # If we're missing a lot of data, then simply assigning 0's skews
+  # the mean and SD. However, short term solution to getting a result.
+  BS[is.na(BS)] <- 0
+  CF[is.na(CF)] <- 0
+  IS[is.na(IS)] <- 0
+  
+  profitability <- collectmarketprofitability(companies, BS, CF, IS)
   growth <- rep(0, numCompanies)
-  for(i in 1:numCompanies){
-    #GPOA = (revenue - cost of goods sold)/(total assets)
-    #?#GROSS PROFITS OVER TOTAL ASSETS. THIS CAN BE EASILY FOUND.
-    #Cost of goods sold = Beginning Inventory + Inventory Purchases - End Inventory
-    ##Gross profit - Income statement
-    ##Total assets - in balance sheet.
-    
-    #ROE
-    # Net income /book equity
-    # Net income - Cash flow
-    #?#Book equity = Total equity (BS) 
-    
-    #ROA
-    #Net income / Total assets
-    # Net income - CF
-    # Total assets - BS
-    
-    #CFOA
-    #(net income + depreciation - (change in working capital) - capital expenditures)/(total assets)
-    # Net income - CF
-    # Depreciation - IS
-    # Change in working capital - CF
-    # Capital Expenditures - CF
-    # Total assets - BS
-    
-    #GMAR
-    # (Revenue - costs of goods sold)/(total sales)
-    # = Gross profit/(total sales)
-    #?# Using different equation:
-    # Gross profit/ (Total revenue)
-    #Gross profit - IS
-    #Total Revenue - IS
-    
-    #ACC
-    # (depreciation - changes in working capital)/(total assets)
-    #*# Going from equation they show. Slight difference from their own
-    ## words.
-    # Depreciation - CF
-    # Changes in working capital - CF
-    # Total assets - BS 
-    
-    ###GROWTH
-    #(5 year change in gross profits)/Total assets
-    
-    #(5 year change in Net income)/book equity
-    
-    #(5 year change in net income)/total assets
-    
-    #(5 year change in cash flow over assets)
+  safety <- rep(0, numCompanies)
+  ###GROWTH
+  #(5 year change in gross profits)/Total assets
+  
+  #(5 year change in Net income)/book equity
+  
+  #(5 year change in net income)/total assets
+  
+  #(5 year change in cash flow over assets)
 
-    #(5 year change in gross profit)/(total sales)
-    
-    #(5 year change in (low) accruals)/total assets
-    
-    ###SAFETY
-    #BAB
-    # - market beta
-  }
+  #(5 year change in gross profit)/(total sales)
+  
+  #(5 year change in (low) accruals)/total assets
+  
+  ###SAFETY
+  #BAB
+  # - market beta
 }
---
+++ R/collectmarketprofitability.R
@@ -0,0 +1,74 @@
+collectmarketprofitability <- function(x, BS, CF, IS){
+  # CollectMarketProfitability collects data on overall profitability
+  ## In the market for individual companies for later processing.
+  ## x is the list of companies to be processed. BS, CF, IS are financial statements.
+  numCompanies <- length(x$tickers)
+  profitability <- rep(0, numCompanies)
+  GPOA <- rep(0, numCompanies)
+  ROE <- rep(0, numCompanies)
+  ROA <- rep(0, numCompanies)
+  CFOA <- rep(0, numCompanies)
+  GMAR <- rep(0, numCompanies)
+  ACC <- rep(0, numCompanies)
+  for(i in 1:numCompanies){
+    cBS <- BS[,4 * (i-1)]
+    cCF <- CF[,4 * (i-1)]
+    cIS <- IS[,4 * (i-1)]
+    #GPOA = (revenue - cost of goods sold)/(total assets)
+    #?#GROSS PROFITS OVER TOTAL ASSETS. THIS CAN BE EASILY FOUND.
+    #Cost of goods sold = Beginning Inventory + Inventory Purchases - End Inventory
+    ##Gross profit - Income statement
+    ##Total assets - in balance sheet.
+    GPOA[i] = cIS[5]/cBS[17]
+    #ROE
+    # Net income /book equity
+    # Net income - Cash flow
+    #?#Book equity = Total equity (BS) 
+    ROE[i] = cCF[1]/cBS[39]
+    #ROA
+    #Net income / Total assets
+    # Net income - CF
+    # Total assets - BS
+    ROA[i] = cCF[1]/cBS[17]
+    
+    #CFOA
+    #(net income + depreciation - (change in working capital) - capital expenditures)/(total assets)
+    # Net income - CF
+    # Depreciation - IS
+    # Change in working capital - CF
+    # Capital Expenditures - CF
+    # Total assets - BS
+    CFOA[i] = (cCF[1] + cIS[8] - cCF[6] - cCF[8])/cBS[17]
+    
+    #GMAR
+    # (Revenue - costs of goods sold)/(total sales)
+    # = Gross profit/(total sales)
+    #?# Using different equation:
+    # Gross profit/ (Total revenue)
+    #Gross profit - IS
+    #Total Revenue - IS
+    GMAR[i] = cIS[5]/cIS[3]
+    
+    #ACC
+    # (depreciation - changes in working capital)/(total assets)
+    #*# Going from equation they show. Slight difference from their own
+    ## words.
+    # Depreciation - CF
+    # Changes in working capital - CF
+    # Total assets - BS 
+    ACC[i] = (cIS[8] - cCF[6])/cBS[17]
+  }
+  
+  #Scale converts the individual scores for these values into z-scores.
+  GPOA <- scale(GPOA)
+  ROE <- scale(ROE)
+  ROA <- scale(ROA)
+  CFOA <- scale(CFOA)
+  GMAR <- scale(GMAR)
+  ACC <- scale(ACC)
+  
+  for(i in 1:numCompanies){
+    profitability[i] <- GPOA[i] + ROE[i] + ROA[i] + CFOA[1] + GMAR[1] + ACC[1]
+  }
+  scale(profitability) 
+}
