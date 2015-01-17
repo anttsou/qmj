66820e506d234ef43b7030f4106afd9610c65bc9
Anthoney Tsou <anttsou@gmail.com>
2015-01-16 19:41:35 -0500

    can now do ticker to name to cik mappings and have a sample of 20 in a csv file

--- getcompanies.R
+++ getcompanies.R
@@ -3,10 +3,12 @@ getcompanies <- function() {
   newLetters <- c("num",letters)
   stringfront <- "http://www.investorguide.com/stock/stocklist"
   stringback <- ".html"
+  urlFront <- "http://www.sec.gov/cgi-bin/cik.pl.c?company="
   names <- NULL
   tickers <- NULL
+  ciks <- NULL
   while(i <= length(newLetters)) {
-    newUrl <- paste(stringfront,paste(letters[i],stringback,sep=""),sep="")
+    newUrl <- paste(stringfront,paste(newLetters[i],stringback,sep=""),sep="")
     con <- url(newUrl)
     htmlCode <- readLines(con)
     close(con)
@@ -17,6 +19,16 @@ getcompanies <- function() {
         newTicker <- gsub("[^A-Za-z]","",sub("\">.*","",sub(".*ticker=","",htmlCode[n+3])))
         names <- c(names,newString)
         tickers <- c(tickers,newTicker)
+        newString2 <- sub(" ", "%20", gsub("[[:punct:]]","",newString))
+        newUrl2 <- paste(urlFront,newString2,sep="")
+        con2 <- url(newUrl2)
+        htmlCode2 <- readLines(con2)
+        close(con2)
+        if(sub("</strong>.*","", sub(".*<strong>","",htmlCode2[9])) != "0") {
+          ciks <- c(ciks,sub("</a>.*","",sub(".*\">","", htmlCode2[10])))
+        } else {
+          ciks <- c(ciks,"N/A")
+        }
       }
       n = n+16
     }
@@ -24,10 +36,12 @@ getcompanies <- function() {
   }
   smallNames <- NULL
   smallTickers <- NULL
+  smallCiks <- NULL
   indices <- c(8482,1586,6338,9087,1297,8674,6081,7800,8079,4854,3381,2469,5203,2841,7576,769,9199,2990,9122,3100)
   for(i in indices) {
     smallNames <- c(smallNames,names[i])
     smallTickers <- c(smallTickers,tickers[i])
+    smallCiks <- c(smallCiks,ciks[i])
   }
-  data.frame(names = smallNames, tickers = smallTickers)
+  write.csv(data.frame(names = smallNames, tickers = smallTickers, ciks = smallCiks),"testcsv2.csv")
 }
