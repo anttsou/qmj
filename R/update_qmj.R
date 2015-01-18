d876926a011a46d8eb246b1192cbfb8e2d96c2f5
Ryan Kwon <rynkwn@gmail.com>
2015-01-18 01:54:12 -0500

    Debugging 'cannot open connection' error with package.

--- R/update_qmj.R
+++ R/update_qmj.R
@@ -6,11 +6,17 @@
 update_qmj <- function(){
   print(paste("Downloading data to: ", system.file("data", package="qmj"), sep=''))
   companies <- qmj::getcompanies()
-  qmj::getdailydata(companies)
+  print("test")
   filepath <- system.file("data", package="qmj")
   filepath <- paste(filepath, "/companies.csv", sep='')
+  print("test")
   write.csv(companies, file=filepath)
+  qmj::getdailydata(companies)
+  print("test")
   qmj::getbalancesheets()
+  print("test")
   qmj::getcashflows()
+  print("test")
   qmj::getincomestatements()
+  print("test")
 }
