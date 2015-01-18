b8945589546d059e4307e692d4277dfb0e2a3e41
Ryan Kwon <rynkwn@gmail.com>
2015-01-18 01:26:07 -0500

    Created very simple overall help screen for the package.

---
+++ R/qmj.R
@@ -0,0 +1,9 @@
+#' Quality Minus Junk
+#'
+#' Calculates and manipulates data on the quality of companies.
+#' 
+#' This package is currently still in early development.
+#' 
+#' @docType package
+#' @name qmj
+NULL
