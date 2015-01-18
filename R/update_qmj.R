22482032cf9f545588176e5dae4be614720747a3
Ryan Kwon <rynkwn@gmail.com>
2015-01-18 01:43:08 -0500

    Fixed update_qmj to now write the companies.csv to the package's data folder.

--- R/update_qmj.R
+++ R/update_qmj.R
@@ -4,10 +4,13 @@
 #' @export
 
 update_qmj <- function(){
+  print(paste("Downloading data to: ", system.file("data", package="qmj"), sep=''))
   companies <- qmj::getcompanies()
   qmj::getdailydata(companies)
+  filepath <- system.file("data", package="qmj")
+  filepath <- paste(filepath, "/companies.csv", sep='')
+  write.csv(companies, file=filepath)
   qmj::getbalancesheets()
   qmj::getcashflows()
   qmj::getincomestatements()
-  print(paste("Downloading data to: ", system.file("data", package="qmj"), sep=''))
 }
