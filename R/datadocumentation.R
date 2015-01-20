d2f3128a1b252bf46b0e4708880bba91bb0823b1
Ryan Kwon <rynkwn@gmail.com>
2015-01-20 05:39:27 -0500

    Created documentation for datasets in datadocuments.R

---
+++ R/datadocumentation.R
@@ -0,0 +1,62 @@
+#' Balancesheets of all companies
+#'
+#' A dataset containing all balancesheets, if available,
+#'  for all companies in the companies.RData file. Among other things,
+#'  stores:
+#'  \itemize{
+#'    \item Cash & Equivalents.
+#'    \item Short Term Investments.
+#'    \item Total Inventory.
+#'    \item Prepaid Expenses.
+#'    \item Total Current Assets.
+#'  }
+#'
+#' @name balancesheets
+#' @docType data
+#' @keywords data
+NULL
+
+#' Cashflow statements of all companies
+#'
+#' A dataset containing all cashflow statements, if available,
+#'  for all companies in the companies.RData file. Among other things,
+#'  stores:
+#'  \itemize{
+#'    \item Net Income/Starting Line.
+#'    \item Depreciation/Depletion.
+#'    \item Amortization.
+#'    \item Deferred Taxes.
+#'    \item Changes in Working Capital.
+#'  }
+#'
+#' @name cashflows
+#' @docType data
+#' @keywords data
+NULL
+
+#' Income statements of all companies
+#'
+#' A dataset containing all income statements, if available,
+#'  for all companies in the companies.RData file. Among other things,
+#'  stores:
+#'  \itemize{
+#'    \item Total Revenue.
+#'    \item Gross Profit.
+#'    \item Research & Development.
+#'    \item Selling/General/Admin. Expenses, Total.
+#'    \item Operating Income.
+#'  }
+#'
+#' @name incomestatements
+#' @docType data
+#' @keywords data
+NULL
+
+#' A list of all companies of interest
+#'
+#' Stores all companies of interest. Stores names and tickers.
+#'
+#' @name companies
+#' @docType data
+#' @keywords data
+NULL
