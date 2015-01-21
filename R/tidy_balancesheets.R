9e1f6ca4e544aefa6e873eccffa78a2147620871
Ryan Kwon <rynkwn@gmail.com>
2015-01-21 15:10:48 -0500

    Added tidy data sets to /data folder. Moved raw dat sets to inst/extdata folder.

--- R/tidy_balancesheets.R
+++ R/tidy_balancesheets.R
@@ -6,15 +6,15 @@
 
 tidy_balancesheets <- function(x) {
   numCompanies <- length(x)
-  balancesheets <- data.frame(matrix(nrow=1, ncol=45))
+  balancesheets <- data.frame(matrix(nrow=1, ncol=44))
   
-  colnames(balancesheets) <- c("name", "ticker", "year", "CE", "STI", "CSTI", "AR", "RE", "TR", "TI", "PE", "OCA", "TCA", "PPE", "AD", "GDW", "INT", "LTI", "OLTA", "TA", "AP", "AE", "STD", "CL", "OCL", "TCL", "LTD", "CLO", "TLTD", "TD", "DIT", "MI", "OL", "TL", "RPS", "NRPS", "CS", "APIC", "RE", "TS", "OE", "TE", "TLSE", "SO", "TCSO")
+  colnames(balancesheets) <- c("ticker", "year", "CE", "STI", "CSTI", "AR", "RE", "TR", "TI", "PE", "OCA", "TCA", "PPE", "AD", "GDW", "INT", "LTI", "OLTA", "TA", "AP", "AE", "STD", "CL", "OCL", "TCL", "LTD", "CLO", "TLTD", "TD", "DIT", "MI", "OL", "TL", "RPS", "NRPS", "CS", "APIC", "RE", "TS", "OE", "TE", "TLSE", "SO", "TCSO")
   for(i in 1:numCompanies){
     cdata <- x[[i]]
     ticker <- gsub('[0-9 ]', '', colnames(cdata))[1]
     years <- gsub('[ABCDEFGHIJKLMNOPQRSTUVWXYZ ]', '', colnames(cdata))
     for(k in 1:length(colnames(cdata))){
-      row <- c("", ticker, years[k], as.numeric(cdata[,k]))
+      row <- c(ticker, years[k], as.numeric(cdata[,k]))
       balancesheets <- rbind(balancesheets, setNames(row, names(balancesheets)))
     }
   }
--- R/tidy_incomestatements.R
+++ R/tidy_incomestatements.R
@@ -6,15 +6,15 @@
 
 tidy_incomestatements <- function(x) {
   numCompanies <- length(x)
-  incomestatements <- data.frame(matrix(nrow=1, ncol=52))
+  incomestatements <- data.frame(matrix(nrow=1, ncol=51))
   
-  colnames(incomestatements) <- c("name", "ticker", "year", "REV", "OREV", "TREV", "CREV", "GPROF", "SGAE", "RD", "DP", "NINT", "UI", "OOE", "TOE", "OI", "INT", "GSA", "OTH", "IBT", "IAT", "MI", "EIA", "NIBEI", "AC", "DO", "EI", "NI", "PD", "IACEEI", "IACIEI", "BWAS", "BEPSEEI", "BEPSIEI", "DILADJ", "DILWAS", "DILEPSEEI", "DILEPSIEI", "DIVC", "GDIV", "NIASBCE", "BEPSSBCE", "DEPSSBCE", "DPSUP", "TSI", "NIBT", "ESIIT", "ITISI", "NIAT", "NIAC", "BNEPS", "DNEPS")
+  colnames(incomestatements) <- c("ticker", "year", "REV", "OREV", "TREV", "CREV", "GPROF", "SGAE", "RD", "DP", "NINT", "UI", "OOE", "TOE", "OI", "INT", "GSA", "OTH", "IBT", "IAT", "MI", "EIA", "NIBEI", "AC", "DO", "EI", "NI", "PD", "IACEEI", "IACIEI", "BWAS", "BEPSEEI", "BEPSIEI", "DILADJ", "DILWAS", "DILEPSEEI", "DILEPSIEI", "DIVC", "GDIV", "NIASBCE", "BEPSSBCE", "DEPSSBCE", "DPSUP", "TSI", "NIBT", "ESIIT", "ITISI", "NIAT", "NIAC", "BNEPS", "DNEPS")
   for(i in 1:numCompanies){
     cdata <- x[[i]]
     ticker <- gsub('[0-9 ]', '', colnames(cdata))[1]
     years <- gsub('[ABCDEFGHIJKLMNOPQRSTUVWXYZ ]', '', colnames(cdata))
     for(k in 1:length(colnames(cdata))){
-      row <- c("", ticker, years[k], as.numeric(cdata[,k]))
+      row <- c(ticker, years[k], as.numeric(cdata[,k]))
       incomestatements <- rbind(incomestatements, setNames(row, names(incomestatements)))
     }
   }
