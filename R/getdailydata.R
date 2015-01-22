966d2567dff7fef95623854defd7335811e6a83b
Ryan Kwon <rynkwn@gmail.com>
2015-01-21 21:08:10 -0500

    very, /very/ rough version of new getdailydata function and data out. getdailydata now calculates adjusted price for S&P 500, saves both price and price returns. dailydata.RData now stored (temporarily) in /data Rough draft of vignette started.

--- R/getdailydata.R
+++ R/getdailydata.R
@@ -14,15 +14,20 @@ getdailydata <- function(x){
   numCompanies <- length(x$tickers)
   thisYear <- as.numeric(format(Sys.Date(), "%Y"))
   desiredDates <- paste(thisYear - 5, "/", sep='')
-  listfiles <- rep("", numCompanies + 1)
+  listfiles <- rep("", 2*(numCompanies + 1))
   #Block of code below specially gathers the daily data for the S&P 500 for use as a benchmark.
   stockData <- quantmod::getSymbols("^GSPC", src="yahoo", auto.assign=FALSE)
   stockData <- stockData[desiredDates,6]
+  priceData <- stockData[,1]
   #stockData <- round(TTR::ROC(quantmod::Cl(stockData)), digits=5)
   stockData[,1] <- qmj::pricereturns(stockData)
   fileName <- paste(filepath, "/", "GSPC.RData", sep='')
+  fileName2 <- paste(filepath,"/", "GSPCprice.RData", sep='')
   listfiles[1] <- fileName
+  listfiles[2] <- fileName2
+  
   save(stockData, file=fileName)
+  save(priceData, file=fileName2)
   for(i in 1:numCompanies){
     companyTicker <- as.character(x$ticker[i])
     print(companyTicker)
@@ -31,31 +36,44 @@ getdailydata <- function(x){
       error=function(e) e
     )
     if(!inherits(stockData, "error") && length(stockData[,1]) > 1 && length(stockData[desiredDates,4]) > 1){
-      stockData <- stockData[desiredDates,6]
+      stockData <- stockData[desiredDates,4]
+      priceData <- stockData
       #Calculates price returns. Not total returns.
       #stockData <- round(TTR::ROC(quantmod::Cl(stockData)), digits=5)
       stockData[,1] <- qmj::pricereturns(stockData)
       fileName <- paste(filepath, "/", companyTicker, ".RData", sep='')
-      listfiles[i+1] <- fileName
+      fileName2 <- paste(filepath, "/", companyTicker, "price", ".RData", sep='')
+      listfiles[2*i + 1] <- fileName
+      listfiles[2*i + 2] <- fileName2
+      
       save(stockData, file=fileName)
+      save(priceData, file=fileName2)
     } else{
       print(paste("Error retrieving data for ", companyTicker, sep=""))
       fileName <- paste(filepath, "/", companyTicker, ".RData", sep='')
-      listfiles[i+1] <- fileName
+      fileName2 <- paste(filepath, "/", companyTicker, "price", ".RData", sep='')
+      listfiles[2*i + 1] <- fileName
+      listfiles[2*i + 2] <- fileName2
       nullData <- rep(NA, 3)
       save(nullData, file=fileName)
+      save(nullData, file=fileName2)
     }
   }
+  print(listfiles)
   compiled <- matrix()
   load(listfiles[1])
+  load(listfiles[2])
   compiled = cbind(compiled, stockData)
-  for(i in 2:length(listfiles)){
-    load(listfiles[i])
+  compiled = cbind(compiled, priceData)
+  for(i in 2:numCompanies){
+    load(listfiles[2*i - 1])
+    load(listfiles[2*i])
     compiled = cbind(compiled, stockData)
+    compiled = cbind(compiled, priceData)
   }
   file.remove(listfiles)
   dailydata <- data.frame(compiled)
   filepath2 <- paste(filepath, "/dailydata.RData", sep='')
-  #save(dailydata, file=filepath2)
+  save(dailydata, file="dailydata.RData")
   dailydata
 }
