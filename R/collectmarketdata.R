b020edb4155baf3f1864e5eca0f39e1748ea1584
Anthoney Tsou <anttsou@gmail.com>
2015-01-24 20:26:07 -0500

    updated git ignore, ordered quality scores, and added a summary function of best and worst companies based on quality

--- R/collectmarketdata.R
+++ R/collectmarketdata.R
@@ -14,11 +14,14 @@ collectmarketdata <- function(){
   data(tidybalance, package="qmj")
   data(tidycash, package="qmj")
   data(tidyincome, package="qmj")
+  data(tidydaily, package="qmj")
+  data(extrafin, package="qmj")
   #companies <- read.csv(paste(filepath, "/companies.csv", sep=''))
   numCompanies <- length(companies$tickers)
   BS <- tidybalance
   CF <- tidycash
   IS <- tidyincome
+  daily <- tidydaily
   
   #What to do with missing data?
   # If we're missing a lot of data, then simply assigning 0's skews
@@ -29,11 +32,20 @@ collectmarketdata <- function(){
   
   profitability <- qmj::collectmarketprofitability(companies, BS, CF, IS)
   growth <- qmj::collectmarketgrowth(companies, BS, CF, IS)
-  safety <- rep(NaN, numCompanies)
-  #safety <- qmj::collectmarketsafety(companies, BS, CF, IS)
-  payouts <- qmj::collectmarketpayout(companies, BS, CF, IS)
+  safety <- qmj::collectmarketsafety(companies, BS, CF, IS, extrafin, daily)
+  payouts <- qmj::collectmarketpayout(companies, BS, IS)
+  quality <- profitability + growth + safety + payouts
   
   names <- companies$names
   tickers <- companies$tickers
-  data.frame(names, tickers, profitability, growth, safety, payouts)
+  marketdata <- data.frame(names = names, 
+                           tickers = tickers, 
+                           profitability = profitability, 
+                           growth = growth, 
+                           safety = safety, 
+                           payouts = payouts, 
+                           quality = quality)
+  marketdata <- marketdata[order(marketdata$quality,decreasing=TRUE,na.rm=TRUE),]
+  filepath2 <- paste(filepath, "/data/marketdata.RData", sep='')
+  save(marketdata,file=filepath2)
 }
