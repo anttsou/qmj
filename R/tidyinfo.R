eaa296ddaa242e4eb8dcb0d5b64a5c6b9b27c32d
Ryan Kwon <rynkwn@gmail.com>
2015-01-26 01:01:56 -0500

    Updated tidy financial data sets. Fixed a bug in tidyinfo.

--- R/tidyinfo.R
+++ R/tidyinfo.R
@@ -12,17 +12,17 @@ tidyinfo <- function(){
   destpathb <- paste(destpath, "/tidybalance.RData", sep='')
   load(filepathb)
   tidybalance <- qmj::tidy_balancesheets(balancesheets)
-  save(tidy_balance, file=destpathb)
+  save(tidybalance, file=destpathb)
   
   filepathc <- paste(filepath, "/cashflows.RData", sep='')
   destpathc <- paste(destpath, "/tidycash.RData", sep='')
   load(filepathc)
   tidycash <- qmj::tidy_cashflows(cashflows)
-  save(tidy_cash, file=destpathc)
+  save(tidycash, file=destpathc)
   
   filepathi <- paste(filepath, "/incomestatements.RData", sep='')
   destpathi <- paste(destpath, "/tidyincome.RData", sep='')
   load(filepathi)
   tidyincome <- qmj::tidy_incomestatements(incomestatements)
-  save(tidy_income, file=destpathi)
+  save(tidyincome, file=destpathi)
 }
