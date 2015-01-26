7b8de5f1191cc36cebd4f10c1a908dc0e4a505ae
Ryan Kwon <rynkwn@gmail.com>
2015-01-26 01:59:09 -0500

    Market cap type for graph_portfolio filled out. More documentation in graph_portfolio

--- R/graph_portfolio.R
+++ R/graph_portfolio.R
@@ -1,18 +1,28 @@
 #' Graphical summary of quality scores
 #'
-#' Returns a graphical summary of the portfolio
+#' Returns a graphical summary of the portfolio. Be aware, missing values, or insufficient information, will result in some values
+#' becoming 0.
 #' 
-#' @param type 
+#' @param type Type that determines the graph created and the information presented.
 #' @export
 
-graph_portfolio <- function(type="summary"){
-  if(type == "summary"){
+graph_portfolio <- function(type="marketcap"){
+  if(type == "marketcap"){
     library(data.table)
     data(companies, package="qmj")
     data(tidybalance, package="qmj")
     data(tidycash, package="qmj")
     data(tidyincome, package="qmj")
     data(tidydaily, package="qmj")
+    
+    companies <- data.frame(companies$tickers)
+    colnames(companies) <- "ticker"
+    
+    fin <- merge(BS, merge(CF, IS, by=c("ticker", "year")), by=c("ticker", "year"))
+    fin <- fin[order(fin$year, decreasing=TRUE),]
+    fin <- data.table(fin, key="ticker")
+    fstyear <- fin[J(unique(ticker)), mult="first"]
+    fstyear <- merge(companies, fstyear, by="ticker", all.x = TRUE)
     
     tidydaily <- data.table(tidydaily, key="ticker")
     tidydaily <- tidydaily[order(as.Date(tidydaily$date, format="%Y-%m-%d"), decreasing=TRUE),]
@@ -26,7 +36,16 @@ graph_portfolio <- function(type="summary"){
     marketcap <- function(closingprice, tcso){
       closingprice * tcso
     }
-    MC <- mapply(marketcap, as.numeric(as.character(tempframe$close)), as.numeric(as.character(tidybalance$TCSO)))
-    quality <- 
+    MC <- mapply(marketcap, as.numeric(as.character(tempframe$close)), as.numeric(as.character(fstyear$TCSO)))
+    MC[is.na(MC)] <- 0
+    data <- collectmarketdata()
+    quality <- data$quality
+    quality[is.na(quality)] <- 0
+    ggplot2::qplot(MC, quality, colour=quality, xlab="Estimated Market Cap", ylab="Quality Score")
+    
+  } else if(type == "summary"){
+    
+  } else {
+    stop("Incorrect type entered! Check spelling and capitalization.")
   }
 }
