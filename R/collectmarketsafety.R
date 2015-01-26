658be48a31ba80fc89d4f317fe228ebf8faf56f3
Ryan Kwon <rynkwn@gmail.com>
2015-01-25 20:27:14 -0500

    fast collectmarketsafety sans IVOL created.

--- R/collectmarketsafety.R
+++ R/collectmarketsafety.R
@@ -22,7 +22,7 @@ collectmarketsafety <- function(x, BS, CF, IS, extrafin, daily){
   
 #   safety <- rep(0, numCompanies)
 #   BAB <- rep(0, numCompanies)
-   IVOL <- rep(0, numCompanies)
+#   IVOL <- rep(0, numCompanies)
 #   LEV <- rep(0, numCompanies)
 #   O <- rep(0, numCompanies)
 #   Z <- rep(0, numCompanies)
@@ -123,6 +123,9 @@ collectmarketsafety <- function(x, BS, CF, IS, extrafin, daily){
     stockdat <- stockdat[1:smallersize,]
     list(marketdat, stockdat)
   }
+  intwo <- function(ni1, ni2){
+    as.numeric(ni1 > 0 && ni2 > 0)
+  }
 
   #BAB scraped from web
   BAB <- extrafin$betas
@@ -163,106 +166,106 @@ collectmarketsafety <- function(x, BS, CF, IS, extrafin, daily){
                                                                 - as.numeric(as.character(fstyear$TL))
                                                                 - as.numeric(as.character(fstyear$RPS))
                                                                 - as.numeric(as.character(fstyear$NRPS))))
-  TLTA <- (as.numeric(as.character(fstyear$TD)) - as.numeric(as.character(fstyear$MI)) - 
+  TLTA <- (as.numeric(as.character(fstyear$TD)) - as.numeric(as.character(fstyear$NI)) - 
              as.numeric(as.character(fstyear$RPS)) - as.numeric(as.character(fstyear$NRPS)))/ADJASSET
   WCTA <- (as.numeric(as.character(fstyear$TCA)) - as.numeric(as.character(fstyear$TCL)))/ADJASSET
   CLCA <- as.numeric(as.character(fstyear$TCL))/as.numeric(as.character(fstyear$TCA))
   OENEG <- as.numeric(as.character(fstyear$NI)) > as.numeric(as.character(fstyear$TA))
   NITA <- as.numeric(as.character(fstyear$NI))/as.numeric(as.character(fstyear$TA))
   FUTL <- as.numeric(as.character(fstyear$IBT))/as.numeric(as.character(fstyear$TL))
-  INTWO <- as.numeric(as.character(fstyear$NI)) < 0 && as.numeric(as.character(sndyear$NI)) < 0
+  INTWO <- mapply(intwo, as.numeric(as.character(fstyear$NI)), as.numeric(as.character(sndyear$NI)))
   CHIN <- (as.numeric(as.character(fstyear$NI)) - as.numeric(as.character(sndyear$NI)))/
     (abs(as.numeric(as.character(fstyear$NI))) + abs(as.numeric(as.character(sndyear$NI))))
   O <- -(-1.32 - 0.407*log(ADJASSET/100) + 6.03*TLTA - 1.43*WCTA + 0.076*CLCA -
            1.72*OENEG - 2.37*NITA - 1.83*FUTL + 0.285*INTWO - 0.521*CHIN)
 
-  for(i in 1:numCompanies) {
-    print(i/numCompanies)
-    cBS <- BS[BS$ticker == x$tickers[i],]
-    cIS <- IS[IS$ticker == x$tickers[i],]
-    cCF <- CF[CF$ticker == x$tickers[i],]
-#     cBS <- subset(BS,ticker == as.character(x$tickers[i]))
-#     cIS <- subset(IS,ticker == as.character(x$tickers[i]))
-#     cCF <- subset(CF,ticker == as.character(x$tickers[i]))
-    
-    if(nrow(cBS) > 0 && nrow(cIS) > 1 && nrow(cCF) > 0 && !is.na(extrafin$betas[i]) 
-       && !is.na(extrafin$ebitdas[i]) && extrafin$ebitdas[i] != "N/A") {
-      
-      compdata <- daily[daily$ticker == as.character(x$tickers[i]),]
-      #BAB[i] <- extrafin$betas[i]
-    
-      #IVOL 
-      #sumvect <- numeric()
-      market <- daily[daily$ticker == "GSPC",]
-      market <- market[market$date == (as.character(cBS$year[1])),]
-        #subset(subset(daily,ticker=="GSPC"),date == as.character(cBS$year[1]))
-      subcomps <- compdata[compdata$date == (as.character(cBS$year[1])),]
-      #subcomps <- subset(subset(daily,ticker==as.character(x$tickers[i])),date == as.character(cBS$year[1]))
-      minsub <- min(c(length(market$ticker),length(subcomps$ticker)))
-      # assume daily risk free return rate is 0.02 percent
-      market <- market[1:minsub,]
-      subcomps <- subcomps[1:minsub,]
-      
-      excess_return <- mapply(exret, as.numeric(as.character(subcomps$close)), BAB[i], market$close)
-#       for(a in 1:minsub) {
-#         excess_return <- as.numeric(as.character(subcomps$close[a])) - (0.0002 + extrafin$betas[i]*
-#                                                                 (as.numeric(as.character(market$close[a])) -
-#                                                                  0.0002))
-#         sumvect <- c(sumvect,excess_return)
-#       }
-      #IVOL[i] <- sd(sumvect)
-      IVOL[i] <- sd(excess_return)
-#       #LEV
-#       # -(total debt/total assets)
-#       #LEV[i] <- -(as.numeric(cBS$TD[1])/as.numeric(cBS$TA[1]))
+#   for(i in 1:numCompanies) {
+#     print(i/numCompanies)
+#     cBS <- BS[BS$ticker == x$tickers[i],]
+#     cIS <- IS[IS$ticker == x$tickers[i],]
+#     cCF <- CF[CF$ticker == x$tickers[i],]
+# #     cBS <- subset(BS,ticker == as.character(x$tickers[i]))
+# #     cIS <- subset(IS,ticker == as.character(x$tickers[i]))
+# #     cCF <- subset(CF,ticker == as.character(x$tickers[i]))
 #     
-#       #O
-#       #ADJASSET = total assets + .1*(market equity - book equity)
-#       #CPI = 100
-#       #TLTA = (total debt - minority interest - preferred stock)/ADJASSET
-#       #WCTA = (current assets - current liabilities)/current assets
-#       #OENEG = 1 if total liabilities > total assets, 0 otherwise
-#       #NITA = net income/total assets
-#       #FUTL = income before tax/total liabilities
-#       #INTWO = 1 if neither the net income of the current year or 
-#       # nor the net income previous year is positive
-#       #CHIN = (net income current year - net income previous year)/
-#       # (|net income current year| + |net income previous year|)
-# #       sumvect2 <- numeric()
-# #       for(a in 1:length(subcomps)) {
-# #         sumvect2 <- c(sumvect2,as.numeric(as.character(daily$close[a])))
+#     if(nrow(cBS) > 0 && nrow(cIS) > 1 && nrow(cCF) > 0 && !is.na(extrafin$betas[i]) 
+#        && !is.na(extrafin$ebitdas[i]) && extrafin$ebitdas[i] != "N/A") {
+#       
+#       compdata <- daily[daily$ticker == as.character(x$tickers[i]),]
+#       #BAB[i] <- extrafin$betas[i]
+#     
+#       #IVOL 
+#       #sumvect <- numeric()
+#       market <- daily[daily$ticker == "GSPC",]
+#       market <- market[market$date == (as.character(cBS$year[1])),]
+#         #subset(subset(daily,ticker=="GSPC"),date == as.character(cBS$year[1]))
+#       subcomps <- compdata[compdata$date == (as.character(cBS$year[1])),]
+#       #subcomps <- subset(subset(daily,ticker==as.character(x$tickers[i])),date == as.character(cBS$year[1]))
+#       minsub <- min(c(length(market$ticker),length(subcomps$ticker)))
+#       # assume daily risk free return rate is 0.02 percent
+#       market <- market[1:minsub,]
+#       subcomps <- subcomps[1:minsub,]
+#       
+#       excess_return <- mapply(exret, as.numeric(as.character(subcomps$close)), BAB[i], market$close)
+# #       for(a in 1:minsub) {
+# #         excess_return <- as.numeric(as.character(subcomps$close[a])) - (0.0002 + extrafin$betas[i]*
+# #                                                                 (as.numeric(as.character(market$close[a])) -
+# #                                                                  0.0002))
+# #         sumvect <- c(sumvect,excess_return)
 # #       }
-#       # market equity
-#       # price of shares*number of shares
-#       
-#       #total assets + (.1 * (market equity - TLSE - TL + RPS + NRPS))
-#       #ME <- sumvect 2 <- all closing prices for that company for the most recent year.
-#       
-#       ADJASSET <- as.numeric(cBS$TA[1]) + 0.1*(ME - (as.numeric(cBS$TLSE[1]) - 
-#                                                    as.numeric(cBS$TL[1]) - 
-#                                                   (as.numeric(cBS$RPS[1]) + 
-#                                                    as.numeric(cBS$NRPS[1]))))
-#       TLTA <- (as.numeric(cBS$TD[1]) - as.numeric(cBS$MI[1])  - 
-#               (as.numeric(cBS$RPS[1]) + as.numeric(cBS$NRPS[1])))/
-#               ADJASSET
-#     
-#       WCTA <- (as.numeric(cBS$TCA[1]) - as.numeric(cBS$TCL[1]))/ADJASSET
-#       CLCA <- as.numeric(cBS$TCL[1])/as.numeric(cBS$TCA[1])
-#       OENEG <- as.numeric(as.numeric(cBS$TL[1]) > as.numeric(cBS$TA[1]))
-#       NITA <- as.numeric(cIS$NI[1])/as.numeric(cBS$TA[1])
-#       FUTL <- as.numeric(cIS$IBT[1])/as.numeric(cBS$TL[1])
-#       INTWO <- as.numeric(as.numeric(cIS$NI[1]) < 0 && as.numeric(cIS$NI[2]) < 0)
-#       CHIN <- (as.numeric(cIS$NI[1]) - as.numeric(cIS$NI[2]))/
-#               (abs(as.numeric(cIS$NI[1])) + abs(as.numeric(cIS$NI[2])))
-#       O[i] <- -(-1.32 - 0.407*log(ADJASSET/100) + 6.03*TLTA - 1.43*WCTA + 0.076*CLCA -
-#                  1.72*OENEG - 2.37*NITA - 1.83*FUTL + 0.285*INTWO - 0.521*CHIN)
-#       
-#       
-     }
-  }
+#       #IVOL[i] <- sd(sumvect)
+#       IVOL[i] <- sd(excess_return)
+# #       #LEV
+# #       # -(total debt/total assets)
+# #       #LEV[i] <- -(as.numeric(cBS$TD[1])/as.numeric(cBS$TA[1]))
+# #     
+# #       #O
+# #       #ADJASSET = total assets + .1*(market equity - book equity)
+# #       #CPI = 100
+# #       #TLTA = (total debt - minority interest - preferred stock)/ADJASSET
+# #       #WCTA = (current assets - current liabilities)/current assets
+# #       #OENEG = 1 if total liabilities > total assets, 0 otherwise
+# #       #NITA = net income/total assets
+# #       #FUTL = income before tax/total liabilities
+# #       #INTWO = 1 if neither the net income of the current year or 
+# #       # nor the net income previous year is positive
+# #       #CHIN = (net income current year - net income previous year)/
+# #       # (|net income current year| + |net income previous year|)
+# # #       sumvect2 <- numeric()
+# # #       for(a in 1:length(subcomps)) {
+# # #         sumvect2 <- c(sumvect2,as.numeric(as.character(daily$close[a])))
+# # #       }
+# #       # market equity
+# #       # price of shares*number of shares
+# #       
+# #       #total assets + (.1 * (market equity - TLSE - TL + RPS + NRPS))
+# #       #ME <- sumvect 2 <- all closing prices for that company for the most recent year.
+# #       
+# #       ADJASSET <- as.numeric(cBS$TA[1]) + 0.1*(ME - (as.numeric(cBS$TLSE[1]) - 
+# #                                                    as.numeric(cBS$TL[1]) - 
+# #                                                   (as.numeric(cBS$RPS[1]) + 
+# #                                                    as.numeric(cBS$NRPS[1]))))
+# #       TLTA <- (as.numeric(cBS$TD[1]) - as.numeric(cBS$MI[1])  - 
+# #               (as.numeric(cBS$RPS[1]) + as.numeric(cBS$NRPS[1])))/
+# #               ADJASSET
+# #     
+# #       WCTA <- (as.numeric(cBS$TCA[1]) - as.numeric(cBS$TCL[1]))/ADJASSET
+# #       CLCA <- as.numeric(cBS$TCL[1])/as.numeric(cBS$TCA[1])
+# #       OENEG <- as.numeric(as.numeric(cBS$TL[1]) > as.numeric(cBS$TA[1]))
+# #       NITA <- as.numeric(cIS$NI[1])/as.numeric(cBS$TA[1])
+# #       FUTL <- as.numeric(cIS$IBT[1])/as.numeric(cBS$TL[1])
+# #       INTWO <- as.numeric(as.numeric(cIS$NI[1]) < 0 && as.numeric(cIS$NI[2]) < 0)
+# #       CHIN <- (as.numeric(cIS$NI[1]) - as.numeric(cIS$NI[2]))/
+# #               (abs(as.numeric(cIS$NI[1])) + abs(as.numeric(cIS$NI[2])))
+# #       O[i] <- -(-1.32 - 0.407*log(ADJASSET/100) + 6.03*TLTA - 1.43*WCTA + 0.076*CLCA -
+# #                  1.72*OENEG - 2.37*NITA - 1.83*FUTL + 0.285*INTWO - 0.521*CHIN)
+# #       
+# #       
+#      }
+#   }
   
   BAB[is.infinite(BAB)] <- 0
-  IVOL[is.infinite(IVOL)] <- 0
+  #IVOL[is.infinite(IVOL)] <- 0
   LEV[is.infinite(LEV)] <- 0
   O[is.infinite(O)] <- 0
   Z[is.infinite(Z)] <- 0
@@ -270,20 +273,20 @@ collectmarketsafety <- function(x, BS, CF, IS, extrafin, daily){
   
   #Scale converts the individual scores for these values into z-scores.
   BAB <- scale(BAB)
-  IVOL <- scale(IVOL)
+  #IVOL <- scale(IVOL)
   LEV <- scale(LEV)
   O <- scale(O)
   Z <- scale(Z)
   EVOL <- scale(EVOL)
   
-  BAB[is.nan(BAB)] <- 0
-  IVOL[is.nan(IVOL)] <- 0
-  LEV[is.nan(LEV)] <- 0
-  O[is.nan(O)] <- 0
-  Z[is.nan(Z)] <- 0
-  EVOL[is.nan(EVOL)] <- 0
+  BAB[is.na(BAB)] <- 0
+  #IVOL[is.nan(IVOL)] <- 0
+  LEV[is.na(LEV)] <- 0
+  O[is.na(O)] <- 0
+  Z[is.na(Z)] <- 0
+  EVOL[is.na(EVOL)] <- 0
   
-  safety <- BAB + IVOL + LEV + O + Z + EVOL
-  #safety <- BAB + LEV + O + Z + EVOL
+  #safety <- BAB + IVOL + LEV + O + Z + EVOL
+  safety <- BAB + LEV + O + Z + EVOL
   scale(safety)
 }
