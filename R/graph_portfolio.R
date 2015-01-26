2a6d7c1dd15de698578e0d1a28d822491e4bcb30
Ryan Kwon <rynkwn@gmail.com>
2015-01-26 00:55:04 -0500

    Made basics of graph_portfolio. Added ggplot2 to imports in DESCRIPTION file.

---
+++ R/graph_portfolio.R
@@ -0,0 +1,11 @@
+#' Graphical summary of quality scores
+#'
+#' Returns a graphical summary of the portfolio
+#' 
+#' @param top The number of top ranked companies to be returned.
+#' @param bottom The number of bottom ranked companies to be returned.
+#' @export
+
+graph_portfolio <- function(top=5, bottom=5){
+  
+}
