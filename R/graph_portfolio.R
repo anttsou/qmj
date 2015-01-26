8732986981df7f9d84da85480f3ee6b5436d8f51
Ryan Kwon <rynkwn@gmail.com>
2015-01-26 01:28:44 -0500

    Began work for graph_portfolio to graph quality vs market cap.

--- R/graph_portfolio.R
+++ R/graph_portfolio.R
@@ -2,10 +2,31 @@
 #'
 #' Returns a graphical summary of the portfolio
 #' 
-#' @param top The number of top ranked companies to be returned.
-#' @param bottom The number of bottom ranked companies to be returned.
+#' @param type 
 #' @export
 
-graph_portfolio <- function(top=5, bottom=5){
-  
+graph_portfolio <- function(type="summary"){
+  if(type == "summary"){
+    library(data.table)
+    data(companies, package="qmj")
+    data(tidybalance, package="qmj")
+    data(tidycash, package="qmj")
+    data(tidyincome, package="qmj")
+    data(tidydaily, package="qmj")
+    
+    tidydaily <- data.table(tidydaily, key="ticker")
+    tidydaily <- tidydaily[order(as.Date(tidydaily$date, format="%Y-%m-%d"), decreasing=TRUE),]
+    setkey(tidydaily, "ticker")
+    tidydaily <- tidydaily[J(unique(ticker)), mult="first"]
+    
+    closingframe <- data.frame(tidydaily$ticker, tidydaily$close)
+    colnames(closingframe) <- c("ticker", "close")
+    tempframe <- merge(companies, closingframe, by='ticker', all.x = TRUE)
+    
+    marketcap <- function(closingprice, tcso){
+      closingprice * tcso
+    }
+    MC <- mapply(marketcap, as.numeric(as.character(tempframe$close)), as.numeric(as.character(tidybalance$TCSO)))
+    quality <- 
+  }
 }
