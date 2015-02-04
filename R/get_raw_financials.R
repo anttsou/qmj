e88664243c75ad58fb668cfbf6d0beed2821b2ca
Ryan Kwon <rynkwn@gmail.com>
2015-02-04 10:38:20 -0500

    Deprecated several functions that retrieve raw data. Cleaned up raw data sets. Removed 'imports data.table' from DESCRIPTION. Roxygenized.

---
+++ R/get_raw_financials.R
@@ -0,0 +1,13 @@
+#' Gets raw financial statements.
+#'
+#' Retrieves raw data for financial statements as returned by the quantmod package.
+#' Structured as a list of three elements, which each element contains either balance sheets,
+#' cash flows, or income statements for all companies.
+#' @export
+
+get_raw_financials <- function(){
+  filepath <- system.file("extdata", package="qmj")
+  filepath <- paste(filepath, "/rawfinancials.RData", sep='')
+  load(filepath)
+  rawfinancials
+}
