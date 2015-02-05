d6025eb6acd9f1bd4fb0ec7254bdf861e0e102be
Anthoney Tsou <anttsou@gmail.com>
2015-02-04 19:48:29 -0500

    added preliminary function to create a new company object

---
+++ R/get_company.R
@@ -0,0 +1,8 @@
+get_company <- function(companies,ticker,financials,prices) {
+  sub.comp <- companies[companies$ticker==ticker,]
+  profitability <- market_profitability(sub.comp,financials)
+  growth <- market_growth(sub.comp,financials)
+  safety <- market_safety(sub.comp,financials,prices)
+  payouts <- market_payout(sub.comp,financials)
+  quality <- profitability + growth + safety + payouts
+}
