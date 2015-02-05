f8089620c6d277e4ec306b425acb2ca838313f56
Anthoney Tsou <anttsou@gmail.com>
2015-02-04 23:31:05 -0500

    slight reformatting of code

--- R/tidyinfo.R
+++ R/tidyinfo.R
@@ -6,26 +6,25 @@
 #' @export
 
 tidyinfo <- function(x){
-#   destpath <- system.file("data", package="qmj")
-#   filepath <- system.file("extdata", package="qmj")
-#   
-#   filepathb <- paste(filepath, "/balancesheets.RData", sep='')
-#   destpathb <- paste(destpath, "/tidybalance.RData", sep='')
-#   load(filepathb)
+  #   destpath <- system.file("data", package="qmj")
+  #   filepath <- system.file("extdata", package="qmj")   
+  #   filepathb <- paste(filepath, "/balancesheets.RData", sep='')
+  #   destpathb <- paste(destpath, "/tidybalance.RData", sep='')
+  #   load(filepathb)
   tidybalance <- tidy_balancesheets(x[[3]])
-#   save(tidybalance, file=destpathb)
+  #   save(tidybalance, file=destpathb)
   
-#   filepathc <- paste(filepath, "/cashflows.RData", sep='')
-# #   destpathc <- paste(destpath, "/tidycash.RData", sep='')
-#   load(filepathc)
+  #   filepathc <- paste(filepath, "/cashflows.RData", sep='')
+  #   destpathc <- paste(destpath, "/tidycash.RData", sep='')
+  #   load(filepathc)
   tidycash <- tidy_cashflows(x[[1]])
-#   save(tidycash, file=destpathc)
+  #   save(tidycash, file=destpathc)
   
-#   filepathi <- paste(filepath, "/incomestatements.RData", sep='')
-# #   destpathi <- paste(destpath, "/tidyincome.RData", sep='')
-#   load(filepathi)
+  #   filepathi <- paste(filepath, "/incomestatements.RData", sep='')
+  #    destpathi <- paste(destpath, "/tidyincome.RData", sep='')
+  #   load(filepathi)
   tidyincome <- tidy_incomestatements(x[[2]])
-#   save(tidyincome, file=destpathi)
+  #   save(tidyincome, file=destpathi)
 
   financials <- merge(tidybalance, merge(tidycash, tidyincome, by=c("ticker", "year")), by=c("ticker", "year"))
   unique(financials)
