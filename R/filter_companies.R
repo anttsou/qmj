0f961ad40a38a4b54145b85ca3822b83a88e99d2
Ryan Kwon <rynkwn@gmail.com>
2015-02-06 20:33:45 -0500

    Finished basic structure of filter_companies. Roxygenized.

--- R/filter_companies.R
+++ R/filter_companies.R
@@ -9,33 +9,58 @@
 #' @param isolate A logical value. If true, returns companies that are driven by the given filter. Mutually exclusive with the remove parameter.
 #' @export
 
-filter_companies <- function(data, filter, remove, isolate){
+filter_companies <- function(data, filter, remove=TRUE, isolate=FALSE){
+  prof_filter <- function(profitability, quality){
+    if(abs(profitability/quality) >= .5)
+      return(TRUE)
+    return(FALSE)
+  }
+  growth_filter <- function(growth, quality){
+    if(abs(growth/quality) >= .5)
+      return(TRUE)
+    return(FALSE)
+  }
+  safety_filter <- function(safety, quality){
+    if(abs(safety/quality) >= .5)
+      return(TRUE)
+    return(FALSE)
+  }
+  payouts_filter <- function(payouts, quality){
+    if(abs(payouts/quality) >= .5)
+      return(TRUE)
+    return(FALSE)
+  }
   if(filter == "profitability"){
+    drivenindices <- mapply(prof_filter, data$profitability, data$quality)
     if(remove){
-      
+      return(data[!drivenindices,])
     } else if (isolate){
-      
+      return(data[drivenindices,])
     }
   } else if(filter == "growth"){
+    drivenindices <- mapply(growth_filter, data$growth, data$quality)
     if(remove){
-      
+      return(data[!drivenindices,])
     } else if (isolate){
-      
+      return(data[drivenindices,])
     }
     
   } else if(filter == "safety"){
+    drivenindices <- mapply(safety_filter, data$safety, data$quality)
     if(remove){
-      
+      return(data[!drivenindices,])
     } else if (isolate){
-      
+      return(data[drivenindices,])
     }
     
   } else if(filter == "payouts"){
+    drivenindices <- mapply(payouts_filter, data$payouts, data$quality)
     if(remove){
-      
+      return(data[!drivenindices,])
     } else if (isolate){
-      
+      return(data[drivenindices,])
     }
-    
+  } else{
+    stop("Error with the filter parameter. Please make sure spelling is correct, and that the filter is lowercase.")
   }
 }
