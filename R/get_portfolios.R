227b6abb6858cd156eb76e068518a23eb8cac3c4
Anthoney Tsou <anttsou@gmail.com>
2015-02-09 20:58:14 -0500

    renamed get_portfolios to get_qmjAnalyses and updated documentation

--- R/get_portfolios.R
+++ R/get_portfolios.R
@@ -1,21 +1,21 @@
-#' Returns a specific Company object (Company Portfolio)
+#' Returns specific qmjAnalysis objects in the existing data set of qmjAnalyses.
 #'
-#' Given a list of tickers and a data frame of portfolios, returns
-#' particular company portfolios based on inputted tickers.
-#' @param tickers A single ticker or vector of tickers that the user desires portfolios for.
-#' @param portfolios A list of company portfolios, i.e. Company Portfolio objects.
+#' Given a vector of tickers and a data frame of qmjAnalyses, returns
+#' particular company qmjAnalyses based on inputted tickers.
+#' @param tickers A single ticker or vector of tickers that the user desires qmjAnalyses for.
+#' @param qmjAnalyses A list of qmjAnalyses, i.e. qmjAnalysis objects.
 #' @examples
 #' tickers <- c("AAPL","GOOG")
-#' data(portfolios)
-#' get_portfolios(tickers,portfolios)
+#' data(qmjAnalyses)
+#' get_qmjAnalyses(tickers,qmjAnalyses)
 #' @export
 
-get_portfolios <- function(tickers, portfolios){
-  get_ticker <- function(portfolio){
-    view_ticker(portfolio)
+get_qmjAnalyses <- function(tickers, qmjAnalyses){
+  get_ticker <- function(qmjAnalysis){
+    view_ticker(qmjAnalysis)
   }
   
-  portfolio_tickers <- sapply(portfolios, get_ticker)
-  indices <- match(tickers, portfolio_tickers)
-  portfolios[indices]
+  qmj_tickers <- sapply(qmjAnalyses, get_ticker)
+  indices <- match(tickers, qmj_tickers)
+  qmjAnalyses[indices]
 }
