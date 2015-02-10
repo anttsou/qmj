dd3c312d34eb80dc7c3f5c114c033e6a035a34d0
Ryan Kwon <rynkwn@gmail.com>
2015-02-09 20:47:44 -0500

    Added more documentation to data sets, aside from portfolios data set.

--- R/datadocumentation.R
+++ R/datadocumentation.R
@@ -1,10 +1,15 @@
-#' Financial statements of all companies for the past four years
+#' Financial statements of all companies in the Russell 3000 index for the past four years
 #'
 #' A data frame containing all financial statements (balancesheets,
-#' cashflows, and income statements) for the past four years if available.
+#' cashflows, and income statements) for the past four years if available. 
 #' 
+#' Some companies may store "weird" data, such as having information solely for the years 1997-2001, or by having multiple 
+#' annual reports within the same year (such as one report being filed in March of 2013, and another filed in December of 2013). In the case of companies
+#' reporting multiple annual data from the same year, the years of their reports are suffixed with their order. For
+#' example, GOOG may have data from 2013.1, 2013.2, 2012.3, 2011.4. This means Google's most recent data set is from
+#' 2013 (2013.1), another data set was published in 2013 (2013.2), and the remaining years are also suffixed for convenience.
 #' 
-#' Data is gathered from Google Finance. Data set contains:
+#' @format A data frame with 11,112 rows and 21 variables
 #'  \itemize{
 #'    \item AM = Amortization
 #'    \item CWC = Changes in Working Capital
@@ -28,16 +33,23 @@
 #'    \item TLSE = Total Liabilities and Shareholders' Equity
 #'    \item TREV = Total Revenue
 #'  }
-#'
+#'  
+#' @source Google Finance, accessed through quantmod
 #' @name financials
 #' @docType data
 #' @keywords data
 NULL
 
-#' A list of all companies of interest
+#' A list of all companies in the Russell 3000 Index.
 #'
-#' Stores the names and tickers for all companies in the Russell 3000 Index.
-#'
+#' Stores the names and tickers for all companies in the Russell 3000 Index as of January 2015.
+#' The list from which the data was culled was last updated 6/27/2014.
+#' @format A data frame with 2999 rows and 2 variables.
+#' \itemize{
+#'    \item name = The name of the company.
+#'    \item ticker = The ticker of the company.
+#'  }
+#' @source \url{https://www.russell.com/documents/indexes/membership/membership-russell-3000.pdf}
 #' @name companies
 #' @docType data
 #' @keywords data
@@ -46,7 +58,7 @@ NULL
 #' A dataframe of price returns and closing prices for companies
 #'
 #' Stores price returns and closing prices for the past two years (if available) for the Russell 3000 Index companies
-#' as well as the S&P 500, to serve as a benchmark.
+#' as well as the S&P 500 (uniquely taken from Yahoo finance), to serve as a benchmark.
 #' 
 #' @format A data frame with 1,475,934 rows and 4 variables
 #' @source Google Finance, accessed through quantmod
@@ -63,11 +75,19 @@ NULL
 #' 
 #' If partial information exists (i.e., a profitability score
 #' was able to be calculated), then those scores are kept for that company, even if
-#' insufficient information exists to produce a quality score.
+#' insufficient information exists to produce a quality score. More details may be found
+#' on the technical vignette.
 #' 
 #' Last updated: January 2015
 #'
 #' @format A data frame with 2999 rows and 7 variables
+#' \itemize{
+#'    \item quality
+#'    \item profitability
+#'    \item growth
+#'    \item safety
+#'    \item payouts
+#'  }
 #'
 #' @name quality
 #' @docType data
@@ -77,7 +97,7 @@ NULL
 #' A dataframe of safety scores for companies listed in the Russell 3000
 #'
 #' Displays safety scores as well as its components for companies in the Russell 3000.
-#' More information may be found on the Technical vignette.
+#' More information may be found on the technical vignette.
 #' Last updated: January 2015
 #' 
 #' @format A data frame with 2999 rows and 8 variables:
@@ -97,7 +117,11 @@ NULL
 
 #' A dataframe of profitability scores for companies listed in the Russell 3000
 #'
-#' Displays profitability scores as well as its components, this includes:
+#' Displays profitability scores as well as its components for companies in the Russell 3000.
+#' More information may be found on the technical vignette.
+#' 
+#' Last updated: January 2015
+#' @format A data frame with 2999 rows and 7 variables:
 #' \itemize{
 #'    \item GPOA = Gross Profits over Assets
 #'    \item ROE = Return on Equity
@@ -107,9 +131,6 @@ NULL
 #'    \item ACC = Accruals
 #'  }
 #'  
-#'  More information may be found on the Technical vignette.
-#' Last updated: January 2015
-#'
 #' @name profitability
 #' @docType data
 #' @keywords data
@@ -117,7 +138,9 @@ NULL
 
 #' A dataframe of growth scores for companies listed in the Russell 3000
 #'
-#' Displays growth scores as well as its components, this includes:
+#' Displays growth scores as well as its components for companies in the Russell 3000.
+#' More information may be found on the technical vignette.
+#' @format A data frame with 2999 rows and 7 variables:
 #' \itemize{
 #'    \item GPOA =  Four year growth in Gross Profits over Assets
 #'    \item ROE = Four year growth in Return on Equity
@@ -126,9 +149,6 @@ NULL
 #'    \item GMAR = Four year growth in Gross Margin
 #'    \item ACC = Four year growth in Accruals
 #'  }
-#'  
-#'  More information may be found on the Technical vignette.
-#' Last updated: January 2015
 #'
 #' @name growth
 #' @docType data
@@ -137,16 +157,15 @@ NULL
 
 #' A dataframe of payout scores for companies listed in the Russell 3000
 #'
-#' Displays payout scores as well as its components, this includes:
+#' Displays growth scores as well as its components for companies in the Russell 3000.
+#' More information may be found on the technical vignette.
+#' @format A data frame with 2999 rows and 5 variables:
 #' \itemize{
 #'    \item EISS = Minus one-year percent change in split-adjusted number of shares.
 #'    \item DISS = Minus one-year percent change in total debt.
 #'    \item NPOP = Sum of total net payout over the past 4 years divided by total profits over the past 4 years.
 #'  }
 #'  
-#'  More information may be found on the Technical vignette.
-#' Last updated: January 2015
-#'
 #' @name payouts
 #' @docType data
 #' @keywords data
