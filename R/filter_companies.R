dc34a1831b14d4f99500de79eac23883f69c7951
Ryan Kwon <rynkwn@gmail.com>
2015-02-09 19:43:30 -0500

    Removed Presentation. Fixed filter_companies and added new filter option 'all'

--- R/filter_companies.R
+++ R/filter_companies.R
@@ -4,32 +4,57 @@
 #' factor. For a company to be driven by a particular component, at least 50% of its quality score must be due to that particular
 #' component. Also allows for the user to trim "driven" companies, returning companies that have scores that are relatively well-distributed.
 #' @param data The qualiy data frame produced by market_data.
-#' @param filter What driver to search for. Valid inputs are "profitability", "growth", "payouts" and "safety".
+#' @param filter What driver to search for. Valid inputs are "profitability", "growth", "payouts", "safety", and "all", which filters companies that are driven by any component.
 #' @param remove A logical value. If true, removes companies from data frame that are driven by the given filter. Mutually exclusive with the isolate parameter.
 #' @param isolate A logical value. If true, returns companies that are driven by the given filter. Mutually exclusive with the remove parameter.
 #' @export
 
 filter_companies <- function(data, filter, remove=TRUE, isolate=FALSE){
   prof_filter <- function(profitability, quality){
-    if(abs(profitability/quality) >= .5)
-      return(TRUE)
-    return(FALSE)
+    if(is.na(profitability) || is.na(quality)){
+      FALSE
+    }else{
+      if(abs(profitability/quality) >= .5){
+        TRUE
+      }else{
+        FALSE
+      }
+    }
   }
   growth_filter <- function(growth, quality){
-    if(abs(growth/quality) >= .5)
-      return(TRUE)
-    return(FALSE)
+    if(is.na(growth) || is.na(quality)){
+      FALSE
+    } else{
+      if(abs(growth/quality) >= .5){
+        TRUE
+      } else{
+        FALSE
+      }
+    }
   }
   safety_filter <- function(safety, quality){
-    if(abs(safety/quality) >= .5)
-      return(TRUE)
-    return(FALSE)
+    if(is.na(safety) || is.na(quality)){
+      FALSE
+    } else{
+      if(abs(safety/quality) >= .5){
+        TRUE
+      } else{
+        FALSE
+      }
+    }
   }
   payouts_filter <- function(payouts, quality){
-    if(abs(payouts/quality) >= .5)
-      return(TRUE)
-    return(FALSE)
+    if(is.na(payouts) || is.na(quality)){
+      FALSE
+    } else{
+      if(abs(payouts/quality) >= .5){
+        TRUE
+      } else{
+        FALSE
+      }
+    }
   }
+  
   if(filter == "profitability"){
     drivenindices <- mapply(prof_filter, data$profitability, data$quality)
     if(remove){
@@ -55,6 +80,19 @@ filter_companies <- function(data, filter, remove=TRUE, isolate=FALSE){
     
   } else if(filter == "payouts"){
     drivenindices <- mapply(payouts_filter, data$payouts, data$quality)
+    if(remove){
+      return(data[!drivenindices,])
+    } else if (isolate){
+      return(data[drivenindices,])
+    }
+  } else if(filter == "all"){
+    drivenindices1 <- mapply(prof_filter, data$profitability, data$quality)
+    drivenindices2 <- mapply(growth_filter, data$growth, data$quality)
+    drivenindices3 <- mapply(safety_filter, data$safety, data$quality)
+    drivenindices4 <- mapply(payouts_filter, data$payouts, data$quality)
+    
+    drivenindices <- as.logical(drivenindices1 + drivenindices2 + drivenindices3 + drivenindices4)
+    
     if(remove){
       return(data[!drivenindices,])
     } else if (isolate){
