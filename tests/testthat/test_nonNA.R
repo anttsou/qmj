c8d989a3db72b3d763ef3f590bae17fefcc0c0bf
Anthoney Tsou <anttsou@gmail.com>
2015-02-17 17:58:29 -0500

    fixed second typo in test_nonNA

--- tests/testthat/test_nonNA.R
+++ tests/testthat/test_nonNA.R
@@ -6,5 +6,5 @@ test_that("calculation returns non-NA value", {
   temp$name[5] <- "test"
   temp$ticker[5] <- "TICK9"
   expect_warning(qmj::market_safety(temp,financials,prices),"BAB for TICK9 generated NA")
-  expect_warning(qmj::market_safety(companies,financials,prices),"IVOL for TICK9 generated NA")
+  expect_warning(qmj::market_safety(temp,financials,prices),"IVOL for TICK9 generated NA")
 })
