65a94d309fceaf03e0df01326ab9a95a5c6ca4c3
Anthoney Tsou <anttsou@gmail.com>
2015-02-27 16:23:16 -0500

    commented out filepath code for now

--- R/get_companies.R
+++ R/get_companies.R
@@ -11,8 +11,8 @@ get_companies <- function() {
   filepath <- system.file("doc",package="qmj")
   filepath <- paste(filepath, "/companies.txt",sep="")
   companies <- read.csv(filepath,stringsAsFactors=FALSE)
-  filepath2 <- system.file("data",package="qmj")
-  filepath2 <- paste(filepath2,"/companies.RData",sep="")
+  #filepath2 <- system.file("data",package="qmj")
+  #filepath2 <- paste(filepath2,"/companies.RData",sep="")
   companies <- companies[,c("ticker","name")]
   companies <- dplyr::arrange(companies, ticker)
   #save(companies,file="~/econ20/R Paper/qmj/data/companies.RData")
