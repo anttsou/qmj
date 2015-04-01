9a3abda06358942a06c5f81205065eec25fbceec
Anthoney Tsou <anttsou@gmail.com>
2015-04-01 11:55:20 -0700

    found errors in get_companies

--- R/get_companies.R
+++ R/get_companies.R
@@ -1,7 +1,7 @@
 #' Gets the company names and tickers from the most recent Russell 3000 Index.
 #' 
-#' Reads in the contents of a text file created from the pdf of company names 
-#' and tickers given by the Russell 3000 Index.
+#' Reads in the contents of a text file created from the pdf of company 
+#' names and tickers given by the Russell 3000 Index.
 #' 
 #' @examples
 #' get_companies()
