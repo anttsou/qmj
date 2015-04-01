889c0c0f11239b0d703048bdcc9fa57fd4f233d0
Anthoney Tsou <anttsou@gmail.com>
2015-04-01 11:20:20 -0700

    added context to tcso test file

--- tests/testthat/test_tcso_positive.R
+++ tests/testthat/test_tcso_positive.R
@@ -1,12 +1,15 @@
+context("Attributes for Financials")
+
+data(companies)
+data(financials)
+data(prices)
+
 test_that("tcso is positive", {
-  data(companies)
-  data(financials)
-  data(prices)
   temp <- financials
   temp <- rbind(temp,rep(-1,length(colnames(temp))))
-  expect_error(qmj::market_data(companies,temp,prices),"Negative TCSO exists.")
-  expect_error(qmj::market_profitability(companies,temp),"Negative TCSO exists.")
-  expect_error(qmj::market_growth(companies,temp),"Negative TCSO exists.")
-  expect_error(qmj::market_safety(companies,temp,prices),"Negative TCSO exists.")
-  expect_error(qmj::market_payouts(companies,temp),"Negative TCSO exists.")
+  expect_error(market_data(companies,temp,prices),"Negative TCSO exists.")
+  expect_error(market_profitability(companies,temp),"Negative TCSO exists.")
+  expect_error(market_growth(companies,temp),"Negative TCSO exists.")
+  expect_error(market_safety(companies,temp,prices),"Negative TCSO exists.")
+  expect_error(market_payouts(companies,temp),"Negative TCSO exists.")
 })
