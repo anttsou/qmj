7c7db754d39e2d19c0856fe9875bfb25484f88af
Anthoney Tsou <anttsou@gmail.com>
2015-07-26 18:45:54 -0400

    required to install qmjdata to use this package

--- R/zzz.R
+++ R/zzz.R
@@ -1,3 +1,5 @@
 .onLoad <- function(libname, pkgname) {
+  if(!require(devtools)) install.packages("devtools")
+  if(!require(qmjdata)) devtools::install_github("anttsou/qmjdata")
   utils::globalVariables(c("desc","year","ticker"))
 }
