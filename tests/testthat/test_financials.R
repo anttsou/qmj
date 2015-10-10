0957cf9d0c6bfcf740b13721034dfa2bd92fce14
Anthoney Tsou <anttsou@gmail.com>
2015-10-10 16:27:54 +0100

    updated readme. still needs work

--- tests/testthat/test_financials.R
+++ tests/testthat/test_financials.R
@@ -6,7 +6,7 @@ prices <- qmjdata::prices
 
 test_that("tcso is positive", {
   temp <- financials
-  temp <- rbind(temp,rep(-1,length(colnames(temp))))
+  temp <- rbind(temp,rep(-1, length(colnames(temp))))
   expect_error(market_data(companies,temp,prices),"Negative TCSO exists.")
   expect_error(market_profitability(companies,temp),"Negative TCSO exists.")
   expect_error(market_growth(companies,temp),"Negative TCSO exists.")
