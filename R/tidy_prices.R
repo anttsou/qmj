dc199377e47f46b9535c44c0a9cc2e6eb7e246b5
Ryan Kwon <rynkwn@gmail.com>
2015-10-10 11:44:17 -0400

    Tidy prices now cleaner. Some commenting begun.

--- R/tidy_prices.R
+++ R/tidy_prices.R
@@ -22,34 +22,25 @@
 #' @export
 
 tidy_prices <- function(x){
-  numCompanies <- (length(names(x))/2) #Every company which has data adds two columns to the raw data output.
-  numDaysInTwoYears <- 732 #We expect at most this many days of data from a company.
-  tidymatrix <- matrix(nrow=(numDaysInTwoYears * numCompanies), ncol=4) #Output matrix is pre-allocated for speed.
-  colnames(tidymatrix) <- c("ticker", "date", "pret", "close")
-  dates <- rownames(x)
-  for(i in 1:numCompanies){
-    ticker <- gsub(".Close","",names(x)[2*i - 1]) #Parses the ticker out of one of the columns contributed by the company.
-    ticker <- gsub(".Adjusted", "", ticker) #Correctly retrieves ticker from the special case of GSPC.Adjusted (S&P 500)
-    col1 <- x[,(2*i)-1]
-    col2 <- x[,(2*i)]
-    
-    #Select only data rows which are not na.
-    nas <- which(!is.na(col1))
-    cDates <- dates[nas]
-    col1 <- col1[nas]
-    col2 <- col2[nas]
-    
-    for(k in 1:length(col1)){
-      #Loop places raw company data into expected positions in pre-allocated tidymatrix.
-      tidymatrix[k + ((i-1) * numDaysInTwoYears), 1] <- ticker
-      tidymatrix[k + ((i-1) * numDaysInTwoYears), 2] <- cDates[k]
-      tidymatrix[k + ((i-1) * numDaysInTwoYears), 4] <- col1[k]
-      tidymatrix[k + ((i-1) * numDaysInTwoYears), 3] <- col2[k]
-    }
+  colNames <- names(x)
+  closing <- x[,grep(".Close", colNames)]
+  pret <- x[,grep("pret", colNames)]
+  
+  #' @describeIn priceCombine combines relevant columns from the original price
+  #' data set.
+  priceCombine <- function(colReference){
+    dates <- rownames(closing)
+    ticker <- gsub(".Close","",names(closing)[colReference])
+    ticker <- rep(ticker, length(dates))
+    cbind(data.frame(ticker, date=dates, pret=pret[,colReference], close=closing[,colReference]))
   }
-  tidyresult <- as.data.frame(tidymatrix, stringsAsFactors=FALSE)
-  tidyresult <- tidyresult[which(!is.na(tidyresult$ticker)), ] #Remove rows that are NA's, which should constitute a large percentage of the data.
-  tidyresult$pret <- as.numeric(tidyresult$pret)
-  tidyresult$close <- as.numeric(tidyresult$close)
-  tidyresult
+  
+  data <- lapply(1:ncol(closing), priceCombine)
+  
+  ## Resulting data frames are combined by rows, and the appropriate
+  ## columns are converted into reasonable types.
+  result <- dplyr::bind_rows(data)
+  result$pret <- as.numeric(result$pret)
+  result$close <- as.numeric(result$close)
+  result
 }
