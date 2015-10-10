1af337c57291b8741389e9d2b71597418af1df86
Anthoney Tsou <anttsou@gmail.com>
2015-10-10 18:03:09 +0100

    added raw_financials as a test dataset

--- tests/testthat/test_tidy_functions.R
+++ tests/testthat/test_tidy_functions.R
@@ -4,22 +4,20 @@ load(system.file("extdata/true_balancesheets.RData", package = "qmj"))
 load(system.file("extdata/true_incomestatements.RData", package = "qmj"))
 load(system.file("extdata/true_cashflows.RData", package = "qmj"))
 load(system.file("extdata/true_info.RData", package = "qmj"))
-
-companies <- qmjdata::companies[1:2,]
-raw_data <- get_info(companies)
+load(system.file("extdata/raw_financials.RData", package = "qmj"))
 
 test_that("balance sheets have expected output", { 
-  expect_equal(tidy_balancesheets(raw_data[[3]]), true_balancesheets)
+  expect_equal(tidy_balancesheets(raw_financials[[3]]), true_balancesheets)
 })
 
 test_that("income statements have expected output", {
-  expect_equal(tidy_incomestatements(raw_data[[2]]), true_incomestatements)
+  expect_equal(tidy_incomestatements(raw_financials[[2]]), true_incomestatements)
 })
 
 test_that("cash flows have expected output", {
-  expect_equal(tidy_cashflows(raw_data[[1]]), true_cashflows)
+  expect_equal(tidy_cashflows(raw_financials[[1]]), true_cashflows)
 })
 
 test_that("all financial statements have merge correctly", {
-  expect_equal(tidyinfo(raw_data), true_info)
+  expect_equal(tidyinfo(raw_financials), true_info)
 })
