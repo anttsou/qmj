13b2bfb6a966da91fc2f51edee40e7009552df1f
Anthoney Tsou <anttsou@gmail.com>
2015-10-20 17:17:47 +0100

    updated tidy helper documentation

--- R/tidy_helper.R
+++ R/tidy_helper.R
@@ -5,29 +5,35 @@
 #' It is used by qmj's tidy functions to reuse common
 #' code and to avoid potential mistakes from repeating
 #' similar processes.
+#' 
+#' @param x A matrix containing financial information, 
+#' either cash flows, balancesheets, or income statements,
+#' downloaded from Google Finance. The formatting of the 
+#' matrix has not been altered yet, as if just retrieved.
+#' 
 #' @seealso \code{\link{tidy_cashflows}}
 #' @seealso \code{\link{tidy_balancesheets}}
 #' @seealso \code{\link{tidy_incomestatements}}
 #' 
 #' @export
 
-tidy_helper <- function(cdata) {
-  
+tidy_helper <- function(x) {
+
   ## Extract the ticker from the first column. 
   ## Remove the unecessary digits to grab only
   ## the ticker letters.
   
-  symbol <- gsub('[0-9 ]', '', colnames(cdata))[1] 
+  symbol <- gsub('[0-9 ]', '', colnames(x))[1] 
   
   ## Extract the years these financial statements were filed. 
   ## Remove the uncessary letters to grab only the year digits.
   
-  years <- gsub('[A-Z]', '', colnames(cdata)) 
+  years <- gsub('[A-Z]', '', colnames(x)) 
   
   ## Takes the transpose to make the rows become columns and appends
   ## the ticker and year columns. 
   
   temp <- cbind(data.frame(ticker = symbol, year = as.numeric(years), 
                            stringsAsFactors = FALSE), 
-                data.frame(t(cdata)))
+                data.frame(t(x)))
 }
