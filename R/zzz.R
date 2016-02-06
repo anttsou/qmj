e77d791e12926e6c333c9b11eefe76c91b63531f
Anthoney Tsou <anttsou@gmail.com>
2016-02-06 23:51:56 +0000

    removed qmjdata from imports

--- R/zzz.R
+++ R/zzz.R
@@ -1,7 +1,8 @@
 .onLoad <- function(libname, pkgname) {
   if (system.file(package = "devtools") == "") 
     install.packages("devtools")
-  if (system.file(package = "qmjdata") == "") 
-    devtools::install_github("anttsou/qmjdata")
+  if (system.file(package = "qmjdata") == "") {
+    devtools::install_github("anttsou/qmjdata", type = "source")
+  }
   utils::globalVariables(c("desc", "year", "ticker"))
 } 
